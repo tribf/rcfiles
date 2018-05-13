d2a749570a53ec5ac7b18b83284e6f4e1c2c71d0
tribf <tristanbf@sina.com>
2018-05-13 15:29:47 +0800

    ADD util

---
+++ shell/utils.sh.d/90-ii.sh
@@ -0,0 +1,22 @@
+#  _        _ _      __
+# | |_ _ __| | |__  / _|
+# | __| '__| | '_ \| |_
+# | |_| |  |_| |_) |  _|
+#  \__|_|  (_)_.__/|_|
+#
+
+function my_ip() {
+    dev=`route -n | grep '^0.0.0.0' | awk '{print $8}'`
+    MY_IP=`ifconfig ${dev} | awk '/inet addr/ {print $2}' | sed -e s/addr://`
+}
+
+function ii() {
+    echo -e "\nYou are logged on ${RED}$HOST"
+    echo -e "\nAdditionnal information:$NC " ; uname -a
+    echo -e "\n${RED}Users logged on:$NC " ; w -h
+    echo -e "\n${RED}Current date :$NC " ; date
+    echo -e "\n${RED}Machine stats :$NC " ; uptime
+    echo -e "\n${RED}Memory stats :$NC " ; free -m
+    my_ip 2>&- ;
+    echo -e "\n${RED}Local IP Address :$NC" ; echo ${MY_IP:-"Not connected"}
+}
