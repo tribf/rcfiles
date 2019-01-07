a6aa35a6e6c002f1493b7d4c94756bc313c722b7
tribf <tristanbf@sina.com>
2019-01-07 23:59:38 +0800

    MOD

---
+++ shell/utils.sh.d/80-nmap.sh
@@ -0,0 +1,12 @@
+#  _        _ _      __
+# | |_ _ __| | |__  / _|
+# | __| '__| | '_ \| |_
+# | |_| |  |_| |_) |  _|
+#  \__|_|  (_)_.__/|_|
+#
+
+
+function scan_rasp() {
+	MY_IP_MASK=`my_ip_mask`
+	sudo nmap -sn $MY_IP_MASK | grep Raspberry -B2
+}
