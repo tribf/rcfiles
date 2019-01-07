66e3479727e49ab3a239c27ce788b6b76e75369c
tribf <tristanbf@sina.com>
2019-01-07 21:43:44 +0800

    MOD

--- shell/utils.sh.d/90-cli_proxy.sh
+++ shell/utils.sh.d/90-cli_proxy.sh
@@ -4,10 +4,12 @@ function cli_proxy() {
 	case $1 in
 		on )
 			export http_proxy=http://127.0.0.1:8123
+			export https_proxy=https://127.0.0.1:8123
 			;;
 
 		off )
 			unset http_proxy
+			unset https_proxy
 			;;
 
 		* )
