363ad743bb0ec38186037a7cf73e1fd01a78c9d0
tribf <tristanbf@sina.com>
2019-01-12 08:25:04 +0800

    ADD

---
+++ shell/utils.sh.d/80-cryptsetup.sh
@@ -0,0 +1,45 @@
+#  _        _ _      __
+# | |_ _ __| | |__  / _|
+# | __| '__| | '_ \| |_
+# | |_| |  |_| |_) |  _|
+#  \__|_|  (_)_.__/|_|
+#
+
+
+function crypt_img() {
+    IMG=/home/tribf/tribfworkspace/tribf.iso
+    MAPPER=/dev/mapper/tribfVolume
+    DISK=/media/tribf/tribf_disk
+
+    case $1 in
+        load )
+            if [[ -e $MAPPER ]]; then
+                echo ""
+                echo "encrypted image($MAPPER) already mapped"
+                echo ""
+            else
+                if [[ -e $IMG ]]; then
+                    sudo cryptsetup -y luksOpen $IMG tribfVolume
+                    sudo mount $MAPPER $DISK
+                    chown tribf:tribf $DISK -R
+                else
+                    echo ""
+                    echo "encrypted image($IMG) not exist"
+                    echo ""
+                fi
+            fi
+            ;;
+        unload )
+            sudo umount $DISK
+            sudo cryptsetup -y luksClose tribfVolume
+            ;;
+        cd )
+            cd $DISK
+            ;;
+        * )
+            echo
+            echo "$0 [load|unload]"
+            echo
+            ;;
+    esac
+}
