98138050cc50b3673c5321080df72f2cf60b8aab
wangyamin <wangyamin@variflight.com>
2019-01-20 00:01:31 +0800

    MOD

--- shell/utils.sh.d/90-ii.sh
+++ shell/utils.sh.d/90-ii.sh
@@ -7,10 +7,13 @@
 
 
 function ii() {
+	cur_data=`date`
+	cur_unixtime=`date -u +%s`
+
     echo -e "\nYou are logged on ${RED}$HOST"
     echo -e "\nAdditionnal information:$NC " ; uname -a
     echo -e "\n${RED}Users logged on:$NC " ; w -h
-    echo -e "\n${RED}Current date :$NC " ; date
+    echo -e "\n${RED}Current date :$NC \n" $cur_unixtime @ $cur_data 
     echo -e "\n${RED}Machine stats :$NC " ; uptime
     echo -e "\n${RED}Memory stats :$NC " ; free -h
     MY_IP=`my_ip`
