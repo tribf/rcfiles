e56e278df27f11ca0f0d2c89b394900cb1f2f52f
wangyamin <wangyamin@variflight.com>
2019-02-11 23:35:52 +0800

    MOD

--- shell/zsh.sh.d/10-autoload.sh
+++ shell/zsh.sh.d/10-autoload.sh
@@ -4,3 +4,4 @@
 #=============================================================================
 autoload -U promptinit && promptinit
 autoload -U compinit && compinit -u
+autoload -U +X bashcompinit && bashcompinit
