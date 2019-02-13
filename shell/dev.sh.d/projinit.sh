675c5ad8f7c41571b8c323eebf54e8c173d8ee76
wangyamin <wangyamin@variflight.com>
2019-02-13 15:02:29 +0800

    MOD

--- shell/dev.sh.d/projinit.sh
+++ shell/dev.sh.d/projinit.sh
@@ -6,18 +6,20 @@ function mvninit() {
   else
       export APP_GROUP_ID=$1
       export JAVA_VERSION=$2
-      TMPL=${TRIBF_SHELL_HOME}/dev.sh.d/templ/mvn/pom.xml.$3.tmpl
+      TMPL_DIR=${TRIBF_SHELL_HOME}/dev.sh.d/templ/mvn
       export APP_ARTIFACT_ID=${APP_GROUP_ID##*.}
-      TARGET=$APP_ARTIFACT_ID/pom.xml
 
       JAVA_PKG_DIR=`echo $APP_GROUP_ID | tr . /`
       mkdir -p $APP_ARTIFACT_ID/src/main/java/$JAVA_PKG_DIR/
       mkdir -p $APP_ARTIFACT_ID/src/test/java/$JAVA_PKG_DIR/
-      envsubst '${APP_GROUP_ID} ${APP_ARTIFACT_ID} ${JAVA_VERSION}' < ${TMPL} > $TARGET
+      mkdir -p $APP_ARTIFACT_ID/conf
+
+      envsubst '${APP_GROUP_ID} ${APP_ARTIFACT_ID} ${JAVA_VERSION}' < ${TMPL_DIR}/pom.xml.$3.tmpl > $APP_ARTIFACT_ID/pom.xml
+      envsubst '${APP_ARTIFACT_ID}' < ${TMPL_DIR}/app.xml.tmpl > $APP_ARTIFACT_ID/conf/app.xml
       cp $TRIBF_SHELL_HOME/dev.sh.d/templ/mvn/log4j.xml $APP_ARTIFACT_ID/log4j.xml
       cp $TRIBF_SHELL_HOME/dev.sh.d/templ/mvn/.gitignore $APP_ARTIFACT_ID/.gitignore
-      cd $APP_ARTIFACT_ID
-      mvn compile
+
+      cd $APP_ARTIFACT_ID && mvn compile
   fi
 }
 
