cf3c826eca6fa80199874f49138dfe76e12ae601
wangyamin <wangyamin@variflight.com>
2019-03-06 09:29:49 +0800

    MOD new function of nmap

--- shell/utils.sh.d/80-nmap.sh
+++ shell/utils.sh.d/80-nmap.sh
@@ -6,7 +6,18 @@
 #
 
 
-function scan_rasp() {
+function nmap_scan_rasp() {
     MY_IP_MASK=`my_ip_mask`
-    sudo nmap -sn $MY_IP_MASK | grep Raspberry -B2
+    sudo nmap -sn --min_parallelism 20 $MY_IP_MASK | grep Raspberry -B2
+}
+
+function nmap_update_mactable() {
+    wget https://raw.githubusercontent.com/nmap/nmap/master/nmap-mac-prefixes && sudo mv nmap-mac-prefixes /usr/share/nmap/nmap-mac-prefixes
+}
+
+function nmap_find_mac_vendor() {
+    for mac in $@; do
+      mac=`echo "$mac" | tr -d ':' | tr -d '-'`
+      grep -i $mac /usr/share/nmap/nmap-mac-prefixes
+    done
 }
