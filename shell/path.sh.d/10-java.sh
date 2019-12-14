86e104b93be25180de1b72a1c38a37ee7ac307ae
tribf <tristanbf@sina.com>
2019-12-14 22:34:04 +0800

    add[path java]: jenv

--- shell/path.sh.d/10-java.sh
+++ shell/path.sh.d/10-java.sh
@@ -19,13 +19,20 @@ export M2_HOME=$HOME/app/apache-maven-3.3.3
 export PATH=$PATH:$M2_HOME/bin
 
 
-export JAVA_HOME=$HOME/app/jdk1.8.0_121
+# export JAVA_HOME=$HOME/app/jdk1.8.0_121
 # export JAVA_HOME=$HOME/app/jdk1.7.0_71
-export CLASSPATH=.:$JAVA_HOME/lib/dt.jar:$JAVA_HOME/lib/tools.jar:$CLASSPATH
-export PATH=$PATH:$JAVA_HOME/bin
+# export CLASSPATH=.:$JAVA_HOME/lib/dt.jar:$JAVA_HOME/lib/tools.jar:$CLASSPATH
+# export PATH=$PATH:$JAVA_HOME/bin
+
+if [ -d ~/.jenv ]; then
+    export PATH="$HOME/.jenv/bin:$PATH"
+    eval "$(jenv init -)"
+else
+    echo "jenv not installed"
+fi
 
 #=============================================================================
 # set path for spring cli
 #=============================================================================
-export SPRING_CLI_HOME=$HOME/app/spring-2.0.4.RELEASE
+export SPRING_CLI_HOME=$HOME/app/spring
 export PATH=$PATH:$SPRING_CLI_HOME/bin
