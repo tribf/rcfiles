21f7fe155ed6e06dbad21495b9fac9ede06ab702
tribf <tristanbf@sina.com>
2020-03-08 10:42:56 +0800

    add[path]: 添加qt相关的配置

---
+++ shell/path.sh.d/10-qt.sh
@@ -0,0 +1,10 @@
+#  _        _ _      __
+# | |_ _ __| | |__  / _|
+# | __| '__| | '_ \| |_
+# | |_| |  |_| |_) |  _|
+#  \__|_|  (_)_.__/|_|
+#
+
+export QTDIR=$HOME/app/Qt5.13.1/5.13.1/gcc_64
+export PATH=$QTDIR/bin:$PATH
+export LD_LIBRARY_PATH=$QTDIR/lib:$LD_LIBRARY_PATH
